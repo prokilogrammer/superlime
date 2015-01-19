9b231a6a00fd1493ede80a4f246502b8d37db3b2
Sanath Kumar Ramesh <sanath@goranku.com>
2015-01-18 17:33:35 -0800

    Rendering a rudimentary keyboard mapping

--- www/modules/editor/editor.module.js
+++ www/modules/editor/editor.module.js
@@ -50,19 +50,270 @@ angular.module('editor.module', [])
 
         $scope.code = "import json\nobj = json.loa";
 
+        $scope.view = 1;
+
+        $scope.keyboardHandler = function(char){
+            console.log("Handler called " , char);
+        }
+
+    }])
+
+    .directive('keyboard', ['$rootScope', '$compile', 'KeymapFactory', function($rootScope, $compile, KeymapFactory){
+
+        var quoted = function(val, escapeQuotes){
+            if (_.isNumber(val)){
+                return val;
+            }
+            else {
+                // Escape any quote characs inside and add quotes to it
+                val = val.replace("'", "\\'");
+                val = val.replace('"', '\\"');
+                return escapeQuotes ? "\\'" + val + "\\'" : val;
+            }
+        }
+
+        var draw = function($scope, element, keymap){
+
+            // Clear the element's contents
+            element.empty();
+
+            // FIXME:  Too many $compiles look horribly inefficient.
+
+            _.forOwn(keymap.views, function(lines, viewName){
+
+                var viewEl = angular.element(_.template('<div class="keyboard-view" ng-class="{hidden: currentKeyboardView != \'${viewName}\' }"></div>', {viewName: quoted(viewName)}));
+                _.forEach(lines, function(line){
+                    var lineEl = angular.element('<div class="row"></div>');
+                    _.forEach(line, function(key){
+                        var keyEl = angular.element(_.template("<div class='col btn' ng-click=\"clicked(\'<%- action %>\', \'<%- value %>\')\"> <%- disp %> </div>", {action: quoted(key.action), value: quoted(key.value), disp: key.disp}));
+                        $compile(keyEl)($scope);
+                        lineEl.append(keyEl);
+                    })
+                    $compile(lineEl)($scope);
+                    viewEl.append(lineEl);
+                });
+
+                $compile(viewEl)($scope);
+                element.append(viewEl);
+            });
+
+        };
+
+        return {
+            restrict: 'E',
+            scope: {
+                type: '@type',
+                handler: '&handler'
+            },
+            link: function($scope, element, attr){
+
+                // Draw the keyboard
+                var keymap = KeymapFactory.get($scope.type);
+                $scope.currentKeyboardView = keymap.meta.startView;
+                draw($scope, element, keymap);
+
+                $scope.clicked = function(action, value){
+
+                    switch(action){
+                        case 'showView':
+                            $scope.currentKeyboardView = value;
+                            break;
+
+                        case 'data':
+                            $scope.handler({char: value});
+                            break;
+
+                        default:
+                            console.log("Unrecognized action ", action, value);
+                    }
+                };
+            }
+        }
+
+    }])
+
+    .factory('KeymapFactory', ['$http', function($http){
+
+        var maps = {
+            'default': {
+                'meta': {
+                    'startView': 'view1'
+                },
+
+                'views': {
+                    'view1':  [
+
+                        // Line 1
+                        [
+                        {disp: 'q', value: 'q', action: 'data'},
+                        {disp: 'w', value: 'w', action: 'data'},
+                        {disp: 'e', value: 'e', action: 'data'},
+                        {disp: 'r', value: 'r', action: 'data'},
+                        {disp: 't', value: 't', action: 'data'},
+                        {disp: 'y', value: 'y', action: 'data'},
+                        {disp: 'u', value: 'u', action: 'data'},
+                        {disp: 'i', value: 'i', action: 'data'},
+                        {disp: 'o', value: 'o', action: 'data'},
+                        {disp: 'p', value: 'p', action: 'data'}
+                        ],
+
+                        // Line 2
+                        [
+                        {disp: 'a', value: 'a', action: 'data'},
+                        {disp: 's', value: 's', action: 'data'},
+                        {disp: 'd', value: 'd', action: 'data'},
+                        {disp: 'f', value: 'f', action: 'data'},
+                        {disp: 'g', value: 'g', action: 'data'},
+                        {disp: 'h', value: 'h', action: 'data'},
+                        {disp: 'j', value: 'j', action: 'data'},
+                        {disp: 'k', value: 'k', action: 'data'},
+                        {disp: 'l', value: 'l', action: 'data'},
+                        {disp: ';', value: ';', action: 'data'}
+                        ],
+
+                        // Line 3
+                        [
+                        {disp: 'z', value: 'z', action: 'data'},
+                        {disp: 'x', value: 'x', action: 'data'},
+                        {disp: 'c', value: 'c', action: 'data'},
+                        {disp: 'v', value: 'v', action: 'data'},
+                        {disp: 'b', value: 'b', action: 'data'},
+                        {disp: 'n', value: 'n', action: 'data'},
+                        {disp: 'm', value: 'm', action: 'data'},
+                        {disp: ',', value: ',', action: 'data'},
+                        {disp: '.', value: '.', action: 'data'},
+                        {disp: '/', value: '/', action: 'data'}
+                        ],
+
+                        // Line 4
+                        [
+                            {disp: 'SHIFT', value: 'view2', action: 'showView'},
+                            {disp: '123', value: 'view3', action: 'showView'},
+                            {disp: 'SPACE', value: ' ', action: 'data'},
+                            {disp: 'BACKSPACE', value: '\b', action: 'data'},
+                            {disp: 'ENTER', value: '\n', action: 'data'}
+                        ]
+                    ],
+
+                    'view2': [
+
+                        // Line 1
+                        [
+                        {disp: 'Q', value: 'Q', action: 'data'},
+                        {disp: 'W', value: 'W', action: 'data'},
+                        {disp: 'E', value: 'E', action: 'data'},
+                        {disp: 'R', value: 'R', action: 'data'},
+                        {disp: 'T', value: 'T', action: 'data'},
+                        {disp: 'Y', value: 'Y', action: 'data'},
+                        {disp: 'U', value: 'U', action: 'data'},
+                        {disp: 'I', value: 'I', action: 'data'},
+                        {disp: 'O', value: 'O', action: 'data'},
+                        {disp: 'P', value: 'P', action: 'data'}
+                        ],
+
+                        // Line 2
+                        [
+                        {disp: 'A', value: 'A', action: 'data'},
+                        {disp: 'S', value: 'S', action: 'data'},
+                        {disp: 'D', value: 'D', action: 'data'},
+                        {disp: 'F', value: 'F', action: 'data'},
+                        {disp: 'G', value: 'G', action: 'data'},
+                        {disp: 'H', value: 'H', action: 'data'},
+                        {disp: 'J', value: 'J', action: 'data'},
+                        {disp: 'K', value: 'K', action: 'data'},
+                        {disp: 'L', value: 'L', action: 'data'},
+                        {disp: ':', value: ':', action: 'data'}
+                        ],
+
+                        // Line 3
+                        [
+                        {disp: 'Z', value: 'Z', action: 'data'},
+                        {disp: 'X', value: 'X', action: 'data'},
+                        {disp: 'C', value: 'C', action: 'data'},
+                        {disp: 'V', value: 'V', action: 'data'},
+                        {disp: 'B', value: 'B', action: 'data'},
+                        {disp: 'N', value: 'N', action: 'data'},
+                        {disp: 'M', value: 'M', action: 'data'},
+                        {disp: '<', value: '<', action: 'data'},
+                        {disp: '>', value: '>', action: 'data'},
+                        {disp: '?', value: '?', action: 'data'}
+                        ],
+
+                        // Line 4
+                        [
+                            {disp: 'SHIFT', value: 'view1', action: 'showView'},
+                            {disp: '123', value: 'view3', action: 'showView'},
+                            {disp: 'SPACE', value: ' ', action: 'data'},
+                            {disp: 'BACKSPACE', value: '\b', action: 'data'},
+                            {disp: 'ENTER', value: '\n', action: 'data'}
+                        ]
+                    ],
+
+                    'view3': [
+                        // Line 1
+                        [
+                        {disp: '1', value: 1, action: 'data'},
+                        {disp: '2', value: 2, action: 'data'},
+                        {disp: '3', value: 3, action: 'data'},
+                        {disp: '4', value: 4, action: 'data'},
+                        {disp: '5', value: 5, action: 'data'},
+                        {disp: '6', value: 6, action: 'data'},
+                        {disp: '7', value: 7, action: 'data'},
+                        {disp: '8', value: 8, action: 'data'},
+                        {disp: '9', value: 9, action: 'data'},
+                        {disp: '0', value: 0, action: 'data'}
+                        ],
+
+                        // Line 2
+                        [
+                        {disp: '-', value: '-', action: 'data'},
+                        {disp: '_', value: '_', action: 'data'},
+                        {disp: '{', value: '}', action: 'data'},
+                        {disp: '$', value: '$', action: 'data'},
+                        {disp: '@', value: '@', action: 'data'},
+                        {disp: '"', value: '"', action: 'data'},
+                        {disp: '\'', value: '\'', action: 'data'},
+                        {disp: '!', value: '!', action: 'data'},
+                        {disp: '[', value: ']', action: 'data'},
+                        {disp: '%', value: '%', action: 'data'}
+                        ],
+
+                        // Line 3
+                        [
+                        {disp: '*', value: '*', action: 'data'},
+                        {disp: '^', value: '^', action: 'data'},
+                        {disp: '+', value: '+', action: 'data'},
+                        {disp: '/', value: '/', action: 'data'},
+                        {disp: '=', value: '=', action: 'data'},
+                        {disp: '|', value: '|', action: 'data'},
+                        {disp: '#', value: '#', action: 'data'},
+                        {disp: '`', value: '`', action: 'data'},
+                        {disp: '&', value: '&', action: 'data'}
+                        ],
+
+                        // Line 4
+                        [
+                            {disp: 'SHIFT', value: 'view1', action: 'showView'},
+                            {disp: '123', value: 'view3', action: 'showView'},
+                            {disp: 'SPACE', value: ' ', action: 'data'},
+                            {disp: 'BACKSPACE', value: '\b', action: 'data'},
+                            {disp: 'ENTER', value: '\n', action: 'data'}
+                        ]
+                    ]
+                }
+            }
+        };
+
+        var factory = {};
+        factory.get = function(type){
+            if (_.has(maps, type)){
+                return maps[type];
+            }
+            else {
+                console.error("Unable to find requested keyboard ", type);
+                return maps['default'];
+            }
+        };
+
+        return factory;
     }]);
-//
-//    .directive('editor', ['$rootScope', function($rootScope){
-//
-//        return {
-//            restrict: 'E',
-//            link: function($scope, element, attr){
-//
-//                // Initialize the editor
-//                var editor = ace.edit(element);
-//                editor.setTheme("ace/theme/twilight");
-//                editor.getSession().setMode("ace/mode/javascript");
-//            }
-//        }
-//
-//    }])
